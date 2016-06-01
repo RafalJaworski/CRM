ac2697f9b1d4b06198e7bf20523075e0d514bde0
RafalJaworski <admin@lux-price.pl>
2016-06-01 08:32:08 +0200

    User new properties

--- src/AppBundle/Entity/User/User.php
+++ src/AppBundle/Entity/User/User.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Entity\User;
 
+use DateTime;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="`user`")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\User\UserRepository")
  */
 class User extends BaseUser
 {
@@ -17,5 +19,232 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-}
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255, nullable=true)
+     */
+    private $lastName;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="mobile", type="string", nullable=true)
+     */
+    private $mobile;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="createdUsers")
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity="User", mappedBy="createdBy")
+     */
+    private $createdUsers;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function preUpdate()
+    {
+        if ((null == $this->createdAt) and (null == $this->updatedAt)) {
+            $this->createdAt = new DateTime();
+            $this->updatedAt = new DateTime();
+        }
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return User
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param \AppBundle\Entity\User\User $createdBy
+     * @return User
+     */
+    public function setCreatedBy(\AppBundle\Entity\User\User $createdBy = null)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return \AppBundle\Entity\User\User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Add createdUsers
+     *
+     * @param \AppBundle\Entity\User\User $createdUsers
+     * @return User
+     */
+    public function addCreatedUser(\AppBundle\Entity\User\User $createdUsers)
+    {
+        $this->createdUsers[] = $createdUsers;
+
+        return $this;
+    }
+
+    /**
+     * Remove createdUsers
+     *
+     * @param \AppBundle\Entity\User\User $createdUsers
+     */
+    public function removeCreatedUser(\AppBundle\Entity\User\User $createdUsers)
+    {
+        $this->createdUsers->removeElement($createdUsers);
+    }
+
+    /**
+     * Get createdUsers
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCreatedUsers()
+    {
+        return $this->createdUsers;
+    }
+}
